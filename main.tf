d966b21830fbea33f7f5f602a9c40673b73a87f1
bgmiprofessor001 <adityakulal2003@gmail.com>
2024-11-21 19:16:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,16 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
+count = 4
 ami = "ami-03753afda9b8ba740"
-instance_type = "t2.large"
+instance_type = "t2.medium"
+key_name = "new1"
+vpc_security_group_ids = ["sg-04ad923cb7e7945b9"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["Dev", "Test", "Monitoring", "Ansible"]
 }
